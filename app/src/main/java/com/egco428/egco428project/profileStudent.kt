0b239b4756db97210dcb5e8e4fb618490263b29b
praowww <praowww@gmail.com>
2018-11-14 14:29:36 +0700

    dialog for student history and student payment

--- app/src/main/java/com/egco428/egco428project/profileStudent.kt
+++ app/src/main/java/com/egco428/egco428project/profileStudent.kt
@@ -3,13 +3,13 @@ package com.egco428.egco428project
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_profile_student.*
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-
-
-
+import android.support.v7.app.AlertDialog
+import android.widget.*
+import kotlinx.android.synthetic.main.payment_dialog.*
+import android.widget.Toast
+import android.widget.CheckBox
 
 
 
@@ -21,6 +21,7 @@ class profileStudent : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_profile_student)
 
         historyBtn.setOnClickListener(this)
+        paymentBtn.setOnClickListener(this)
 
         // Write a message to the database
         val database = FirebaseDatabase.getInstance()
@@ -32,7 +33,51 @@ class profileStudent : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         if(v===historyBtn){
+
+            val names = arrayOf("A", "B", "C", "D")
+            var alertDialog = AlertDialog.Builder(this).create()
+            val inflater = layoutInflater
+            val convertView = inflater.inflate(R.layout.history_dialog, null) as View
+            alertDialog.setView(convertView)
+            val lv = convertView.findViewById<View>(R.id.historyList) as ListView
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, names)
+            lv.setAdapter(adapter)
+            val btn = convertView.findViewById<View>(R.id.okBtn) as Button
+            btn.setOnClickListener {
+                alertDialog.dismiss()
+            }
+            alertDialog.show()
         }
+        else if(v===paymentBtn){
+
+            var alertDialog = AlertDialog.Builder(this).create()
+            val inflater = layoutInflater
+            val convertView = inflater.inflate(R.layout.payment_dialog, null) as View
+            alertDialog.setView(convertView)
+            val ten = convertView.findViewById<View>(R.id.cash_100) as ImageView
+            val btn = convertView.findViewById<View>(R.id.okBtn) as Button
+
+            val check100 = convertView.findViewById<View>(R.id.checkbox_cash100) as CheckBox
+            val check200 = convertView.findViewById<View>(R.id.checkbox_cash200) as CheckBox
+            val check300 = convertView.findViewById<View>(R.id.checkbox_cash300) as CheckBox
+            val check400 = convertView.findViewById<View>(R.id.checkbox_cash400) as CheckBox
+            val check500 = convertView.findViewById<View>(R.id.checkbox_cash500) as CheckBox
+            val check600 = convertView.findViewById<View>(R.id.checkbox_cash600) as CheckBox
+
+            btn.setOnClickListener {
+                var values = 0
+                if(check100.isChecked){values = values+100}
+                if(check200.isChecked){values = values+200}
+                if(check300.isChecked){values = values+300}
+                if(check400.isChecked){values = values+400}
+                if(check500.isChecked){values = values+500}
+                if(check600.isChecked){values = values+600}
+                creditText.text = "Credit : "+values
+                alertDialog.dismiss()
+            }
+            alertDialog.show()
+        }
+
     }
 
 }
