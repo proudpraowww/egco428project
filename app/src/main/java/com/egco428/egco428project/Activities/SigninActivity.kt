75048934aa0056c5160567e9ee3f217342e53516
Sirapop <sirapop.kam@gmail.com>
2018-11-15 15:28:38 +0700

    Signin and Signup - Firebase Auth

---
+++ app/src/main/java/com/egco428/egco428project/Activities/SigninActivity.kt
@@ -0,0 +1,82 @@
+package com.egco428.egco428project.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.widget.Toast
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import android.content.Intent
+import com.egco428.egco428project.R
+import kotlinx.android.synthetic.main.activity_signin.*
+
+class SigninActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+    private var mAuthTest: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        /*val user = mAuth!!.currentUser
+        if(user != null){
+            val intent = Intent(this,profileStudent::class.java)
+            startActivity(intent)
+            finish()
+        }*/
+
+        signinBtn.setOnClickListener{
+
+            var email = usernameText.text.toString()
+            var password = passwordText.text.toString()
+
+            if(email.isEmpty()){
+                usernameText.error = "Please enter your name"
+            }
+            if(password.isEmpty()){
+                passwordText.error = "Please enter your password"
+            }
+            if(!email.isEmpty() && !password.isEmpty()){
+                login(email,password)
+            }
+        }
+
+        signupBtn.setOnClickListener{
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun login(email: String, password: String){
+        mAuth!!.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = mAuth!!.getCurrentUser()
+                        Toast.makeText(applicationContext,"Sigin Success Fully....",Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(applicationContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.getCurrentUser()
+    }
+
+
+}
---
+++ app/src/main/java/com/egco428/egco428project/Activities/SignupActivity.kt
@@ -0,0 +1,77 @@
+package com.egco428.egco428project.Activities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_signup.*
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import android.os.Message
+import android.util.Log
+import com.egco428.egco428project.Model.Member
+import com.egco428.egco428project.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
+class SignupActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+    lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        mAuth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().getReference("Members")
+
+        submitBtn.setOnClickListener{
+            register()
+            //finish()
+        }
+
+        cancleBtn.setOnClickListener{
+            finish()
+        }
+    }
+
+    private fun register(){
+
+        var email = emailText.text.toString()
+        var password = pwdText.text.toString()
+        var name = nameText.text.toString()
+        var lastname = lastnameText.text.toString()
+        var status = statusText.text.toString()
+        var phone = phoneText.text.toString()
+
+        if(!email.isEmpty() && !password.isEmpty() && !name.isEmpty() && !lastname.isEmpty() && !status.isEmpty() && !phone.isEmpty()){
+            mAuth!!.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener{
+                    val messageId = database.push().key
+                    val messageData = Member(messageId!!,email, password, name,lastname,status,phone)
+                    database.child(messageId).setValue(messageData).addOnCompleteListener({
+                        Toast.makeText(applicationContext,"Completely",Toast.LENGTH_SHORT).show()
+                    })
+
+                    val intent = Intent(this, SigninActivity::class.java)
+                    startActivity(intent)
+
+                }
+                .addOnFailureListener{
+                    Toast.makeText(applicationContext,"Failed....",Toast.LENGTH_SHORT).show()
+                    Log.d("Signup Failed",it.printStackTrace().toString())
+                }
+
+        } else{
+            Toast.makeText(this,"Please enter", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
