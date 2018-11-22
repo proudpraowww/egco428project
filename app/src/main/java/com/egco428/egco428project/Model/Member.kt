36c24a1d4c7278bd5228ae53eae4131626f68765
praowww <praowww@gmail.com>
2018-11-22 17:29:28 +0700

    modify member in Member and requestFragment

--- app/src/main/java/com/egco428/egco428project/Model/Member.kt
+++ app/src/main/java/com/egco428/egco428project/Model/Member.kt
@@ -1,6 +1,6 @@
 package com.egco428.egco428project.Model
 
-class Member(val id:String , val email: String, val password: String, val name: String, val lastname: String, val status:String, val phone: String) {
+class Member(val id:String , val email: String, val password: String, val name: String, val lastname: String, val status:String, val phone: String, val school: String, val statusOnOff: String, val latitude: String, val longitude: String, val credit: String) {
 
-    constructor(): this("","","","","","","")
+    constructor(): this("","","","","","","","","","","","")
 }
